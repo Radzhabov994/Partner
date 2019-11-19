e18104039f1cb537e92f54b3e1ab6786137f68f6
Gamid Radzhabov <radzhabov994@gmail.com>
2019-11-19 09:58:15 +0300

    Initial Commit

---
+++ Partner/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Partner
+//
+//  Created by Гамид Раджабов on 19.11.2019.
+//  Copyright © 2019 Gamid Radzhabov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
