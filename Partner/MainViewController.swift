f309184a7b950e3444fc9829cbd5227aa5400178
Gamid Radzhabov <radzhabov994@gmail.com>
2020-04-12 17:22:15 +0300

    Initial

---
+++ Partner/MainViewController.swift
@@ -0,0 +1,52 @@
+//
+//  MainViewController.swift
+//  Partner
+//
+//  Created by Гамид Раджабов on 19.11.2019.
+//  Copyright © 2019 Gamid Radzhabov. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UITableViewController {
+    
+    let partners = ["Yandex", "Vkontakte", "Mail.ru", "Avito", "Telegram"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return partners.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+
+        cell.textLabel?.text = partners[indexPath.row]
+        cell.imageView?.image = UIImage(named: partners[indexPath.row])
+        cell.imageView?.layer.cornerRadius = cell.frame.size.height / 2
+        cell.imageView?.clipsToBounds = true
+
+        return cell
+    }
+    
+    //MARK: - Table view delegate
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 85
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Partner/PartnerModel.swift
@@ -0,0 +1,16 @@
+//
+//  PartnerModel.swift
+//  Partner
+//
+//  Created by Гамид Раджабов on 22.11.2019.
+//  Copyright © 2019 Gamid Radzhabov. All rights reserved.
+//
+
+import Foundation
+
+struct Partner {
+    
+    var name: String
+    var inn: String
+    var location: String
+}
